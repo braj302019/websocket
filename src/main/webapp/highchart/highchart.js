02a4da5dc27fdcab5b5caf5dc50cebe138fb8317
Braj Mohan Jangid <braj.jangid@mycom-osi.com>
2017-12-21 18:10:19 +0530

    templates in main html file using bootstreap carosuel

--- src/main/webapp/highchart/highchart.js
+++ src/main/webapp/highchart/highchart.js
@@ -1,71 +1,68 @@
-(function() {
+$(document).ready(
+		function() {
 
-	Highcharts.setOptions({
-		global : {
-			useUTC : false
-		}
-	});
-
-	// Create the chart
-	var stockChart = Highcharts.stockChart('container', {
-		chart : {
-			events : {
-				load : function() {
-					//nothing
+			Highcharts.setOptions({
+				global : {
+					useUTC : false
 				}
-			}
-		},
+			});
 
-		rangeSelector : {
-			buttons : [ {
-				count : 1,
-				type : 'minute',
-				text : '1M'
-			}, {
-				count : 5,
-				type : 'minute',
-				text : '5M'
-			}, {
-				type : 'all',
-				text : 'All'
-			} ],
-			inputEnabled : false,
-			selected : 0
-		},
+			// Create the chart
+			var stockChart = Highcharts.stockChart('container', {
+				chart : {
+					events : {
+						load : function() {
+							// nothing
+						}
+					}
+				},
 
-		title : {
-			text : 'Live random data'
-		},
+				rangeSelector : {
+					buttons : [ {
+						count : 1,
+						type : 'minute',
+						text : '1M'
+					}, {
+						count : 5,
+						type : 'minute',
+						text : '5M'
+					}, {
+						type : 'all',
+						text : 'All'
+					} ],
+					inputEnabled : false,
+					selected : 0
+				},
 
-		exporting : {
-			enabled : false
-		},
+				title : {
+					text : 'Live random data'
+				},
 
-		series : [ {
-			name : 'Random data',
-			data : (function() {
-				// generate an array of random data
-				var data = [],
-					time = (new Date()).getTime(),
-					i;
+				exporting : {
+					enabled : false
+				},
 
-				for (i = -999; i <= 0; i += 1) {
-					data.push([
-						time + i * 1000,
-						Math.round(Math.random() * 100)
-					]);
-				}
-				return data;
-			}())
-		} ],
-	});
+				series : [ {
+					name : 'Random data',
+					data : (function() {
+						// generate an array of random data
+						var data = [], time = (new Date()).getTime(), i;
 
-	socket = new WebSocket("ws://localhost:8080/websocket/actions/highchart"),
-	socket.onmessage = function(event) {
-		var point = JSON.parse(event.data);
-		var series = stockChart.series[0];
-		var x = new Date(point.x).getTime(),
-			y = point.y;
-		series.addPoint([ x, y ], true, true);
-	};
-})();
+						for (i = -999; i <= 0; i += 1) {
+							data.push([ time + i * 1000,
+									Math.round(Math.random() * 100) ]);
+						}
+						return data;
+					}())
+				} ],
+			});
+
+			socket = new WebSocket(
+					"ws://localhost:8080/websocket/actions/highchart"),
+					socket.onmessage = function(event) {
+						var point = JSON.parse(event.data);
+						var series = stockChart.series[0];
+						var x = new Date(point.x).getTime(), y = point.y;
+						series.addPoint([ x, y ], true, true);
+					};
+		});
--- src/main/webapp/stock/stock.js
+++ src/main/webapp/stock/stock.js
@@ -1,7 +1,8 @@
-(function() {
+$(document).ready(function() {
 	var socket = new WebSocket("ws://localhost:8080/websocket/actions/stock");
 	socket.onmessage = onMessage;
 	var stocks = [];
+	var dataTable = initDataTable();
 
 	function onMessage(event) {
 		var data = JSON.parse(event.data);
@@ -12,9 +13,9 @@
 
 	function renderColumn(data, type, full, meta) {
 		if (full.change < 0) {
-			return '<div style="background-color:#ECE981">' + data + '</div>';
+			return '<div style="background-color:#ECE981" class="background-clear">' + data + '</div>';
 		} else if (data > 0) {
-			return '<div style="background-color:#90EE90">' + data + '</div>';
+			return '<div style="background-color:#90EE90" class="background-clear">' + data + '</div>';
 		} else {
 			return data;
 		}
@@ -31,63 +32,67 @@
 		var index = stocks.findIndex(s => s.id == data.id);
 		if (index == -1) {
 			stocks.push(data);
+			dataTable.fnAddData(data, true);
 		} else {
 			stocks[index] = data;
+			dataTable.fnUpdate(data, index);
 		}
-
-		$(document).ready(function() {
-			var table = $('#stockTable').dataTable({
-				paging : false,
-				searching : false,
-				retrieve : true,
-				data : stocks,
-				columns : [
-					{
-						data : "name"
-					},
-					{
-						data : "price",
-						render : function(data, type, full, meta) {
-							return renderColumn(data, type, full, meta);
-						}
-					},
-					{
-						data : "time",
-						render : function(data, type, full, meta) {
-							return renderColumn(data, type, full, meta);
-						}
-					},
-					{
-						data : "change",
-						render : function(data, type, full, meta) {
-							if (data < 0) {
-								return '<i class="fa fa-caret-down" aria-hidden="true" style="color:red"></i>';
-							} else if (data > 0) {
-								return '<i class="fa fa-caret-up" aria-hidden="true" style="color:green"></i>';
-							} else {
-								return '';
-							}
-						}
-					},
-					{
-						data : "change",
-						render : function(data, type, full, meta) {
-							return renderColumn(data + '%', type, full, meta);
-						}
-					},
-					{
-						data : "min"
-					},
-					{
-						data : "max"
+		
+		
+		if (index != -1) {
+			dataTable.fnUpdate(data, index);
+		}
+	}
+	
+	function initDataTable()
+	{
+		return $('#stockTable').dataTable({
+			paging : false,
+			searching : false,
+			retrieve : true,
+			data : [],
+			columns : [
+				{
+					data : "name"
+				},
+				{
+					data : "price",
+					render : function(data, type, full, meta) {
+						return renderColumn(data, type, full, meta);
 					}
-				]
-			});
-
-			if (index != -1) {
-				table.fnUpdate(data, index, undefined, false);
-			}
+				},
+				{
+					data : "time",
+					render : function(data, type, full, meta) {
+						return renderColumn(data, type, full, meta);
+					}
+				},
+				{
+					data : "change",
+					render : function(data, type, full, meta) {
+						if (data < 0) {
+							return '<i class="fa fa-caret-down" aria-hidden="true" style="color:red"></i>';
+						} else if (data > 0) {
+							return '<i class="fa fa-caret-up" aria-hidden="true" style="color:green"></i>';
+						} else {
+							return '';
+						}
+					}
+				},
+				{
+					data : "change",
+					render : function(data, type, full, meta) {
+						return renderColumn(data + '%', type, full, meta);
+					}
+				},
+				{
+					data : "min"
+				},
+				{
+					data : "max"
+				}
+			]
 		});
 	}
 
-})();
+});
